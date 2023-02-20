bc0c5c4b105f90ab61d3f83d01c40bf19ecc9698
Fernando Cruz <fernandogomescruz@gmail.com>
2023-02-20 00:19:25 -0300

    Added - line that allow to perform more than one require

--- scene.lua
+++ scene.lua
@@ -1,5 +1,10 @@
 function ChangeScene(path, scene)
-    Scene = nil
-    Scene = require(path .. "." .. scene)
+    Scene = Scene or {}
+    if Scene.clean then Scene:clean() end
+
+    newscene = path .. "." .. scene
+    package.loaded[newscene] = false
+    Scene = require(newscene)
+    print(Scene.draw)
     if Scene.load then Scene:load() end
 end
